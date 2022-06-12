efd8e59a76a215a1bc4f9d2f5b5accdcb8bfedf3
Emem122 <knu.ee1202@gmail.com>
2022-06-12 22:26:34 +0900

    UPDATE: update project github url

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -7,7 +7,7 @@ export const projectsData = [
 		tags: ["Next.js", "tailwindCSS", "Framer Motion"],
 		url: "https://en-portfolio.pages.dev",
 		image: "thumb_portfolio",
-		github: "https://github.com",
+		github: "https://github.com/Emem122/portfolio-2022-app",
 	},
 	{
 		id: uuidv4(),
@@ -15,7 +15,7 @@ export const projectsData = [
 		tags: ["HTML", "SCSS", "jQuery", "GSAP"],
 		url: "https://correcta.pages.dev",
 		image: "thumb_correcta",
-		github: "https://github.com",
+		github: "https://github.com/Emem122/correcta",
 	},
 	{
 		id: uuidv4(),
@@ -23,15 +23,7 @@ export const projectsData = [
 		tags: ["HTML", "tailwindCSS", "JS"],
 		url: "https://en-todoapp.pages.dev",
 		image: "thumb_todoapp",
-		github: "https://github.com",
-	},
-	{
-		id: uuidv4(),
-		title: "cocon -Hotel Site",
-		tags: ["HTML", "SCSS", "jQuery"],
-		url: "https://cocon.pages.dev",
-		image: "thumb_cocon",
-		github: "https://github.com",
+		github: "https://github.com/Emem122/my-todo-app",
 	},
 	{
 		id: uuidv4(),
@@ -39,7 +31,15 @@ export const projectsData = [
 		tags: ["React", "styled-component", "Framer Motion"],
 		url: "https://en-portfolio-react.pages.dev",
 		image: "thumb_portfolio_old",
-		github: "https://github.com",
+		github: "https://github.com/Emem122/portfolio-2022",
+	},
+	{
+		id: uuidv4(),
+		title: "cocon -Hotel Site",
+		tags: ["HTML", "SCSS", "jQuery"],
+		url: "https://cocon.pages.dev",
+		image: "thumb_cocon",
+		// github: "https://github.com",
 	},
 	{
 		id: uuidv4(),
@@ -47,6 +47,6 @@ export const projectsData = [
 		tags: ["HTML", "SCSS", "jQuery", "GSAP"],
 		url: "https://earth-jewelry.pages.dev",
 		image: "thumb_earth",
-		github: "https://github.com",
+		// github: "https://github.com",
 	},
 ];
